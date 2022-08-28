a9c68cc1a2537c2db2cb61b69f58614ec92a41c0
mdawood832 <mdawood832@gmail.com>
2022-08-28 00:58:57 -0500

    adding all the new stuff

---
+++ models/quotes.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose') // require mongoose
+// const { values } = require('./seed')
+const Schema = mongoose.Schema
+
+// SCHEMA   
+const quoteSchema = new Schema({
+    title: { type: String},
+    author: { type: String},
+    publishDate: { type: Date, default: Date.now }, 
+    body: { type: String}, 
+
+}, { timestamps: true })
+
+// MODEL 
+const Quote = mongoose.model('Quote', quoteSchema)
+
+// EXPORT   
+module.exports = Quote
+
--- server.js
+++ server.js
@@ -1,16 +1,65 @@
 const express = require('express')
 const app = express()
 const PORT  = 3000
+const Quote = require('./models/quotes.js')
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+const {render} = require('ejs')
+
+// MONGOOSE 
+const mongoose = require('mongoose');
+
+// MONGOOSE CONNECT 
+mongoose.connect('mongodb://127.0.0.1:27017/basiccrud', { useNewUrlParser: true});
+mongoose.connection.once('open', ()=> {
+    console.log('connected to mongo');
+});
 
 // MIDDLEWARE
 app.use(express.static('public'))
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:false}))
+
+// #############################################################
+// ROUTES
+// #############################################################
 
 // INDEX ROUTE  
 app.get('/frenzies' , (req, res) => {
-    res.render('index.ejs')
+    Quote.find({} , (error, allQuotes) => {
+        res.render('index.ejs' , {
+            quotes: allQuotes
+        })
+    })
 })
+
+// SEED
+app.get('/frenzies/seed', (req, res) => {
+	Quote.create(
+		[
+			{
+				title: 'grapefruit',
+				author: 'pink',
+                body: ''
+			},
+			{
+				title: 'grapefruit',
+				author: 'pink',
+                body: ''
+			},
+			{
+				title: 'grapefruit',
+				author: 'pink',
+                body: ''
+			}
+		],
+		(err, data) => {
+			res.redirect('/frenzies');
+		}
+	);
+});
+
+
 
 // ABOUT ROUTE 
 app.get('/frenzies/about' , (req,res) => {
@@ -18,7 +67,49 @@ app.get('/frenzies/about' , (req,res) => {
 }
 )
 
+// NEW ROUTE 
+app.get('/frenzies/new' , (req,res) => {
+    res.render('new.ejs')
+})
+
+// POST ROUTE 
+app.post('/frenzies', (req, res)=>{
+    Quote.create(req.body, (error, createdQuote)=>{
+        if (error){
+        	console.log(error);
+        	res.send(error);
+        }
+        else{
+	        res.send(createdQuote);
+            // res.redirect('/frenzies')
+        }
+    }
+)});
+
+// SHOW ROUTE 
+app.get('/frenzies/:id', (req, res)=>{
+    Quote.findById(req.params.id, (err, foundQuote)=>{
+        res.render('show.ejs' , {
+            quote: foundQuote
+        })
+    });
+});
+
+// DELETE ROUTE 
+app.delete('/frenzies/:id', (req, res)=>{
+    Quote.findByIdAndRemove(req.params.id , (err, data) => {
+        res.redirect('/frenzies')
+    })
+})
+
+
+
+// #############################################################
+// LISTENING 
+// #############################################################
+
 // PORT
 app.listen(PORT, (req,res) => {
     console.log(`listening to sweet beats on ${PORT}`)
 })
+
